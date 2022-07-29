e5c9549e2520ccc9471d67cd3aac83ccd7ea4170
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-07-29 17:18:21 -0300

    Criar arquivo que copiar dados de um para o outro

---
+++ Disciplina-programacao extrema/copia-arquivos.java
@@ -0,0 +1,36 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Informe o nome do arquivo:\n");
+		String caminhoArquivo = scanner.nextLine();
+		InputStream is = null;
+		OutputStream os = null;
+		
+		try {
+			is = new FileInputStream(caminhoArquivo);
+			os = new FileOutputStream("cópia de" + caminhoArquivo);
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = is.read(buffer)) > 0) {
+				os.write(buffer, 0, length);
+			}
+		} 
+		catch (Exception e) {
+			System.out.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+		}
+		
+		finally {
+		is.close();
+		os.close();
+
+		}
+	}
+}
